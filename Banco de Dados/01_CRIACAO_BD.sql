f3c7685becd1b16427b14f5e8aee6ffeecbf45b5
Marcos Hilario <mrhilario1985@gmail.com>
2020-03-10 14:39:21 -0300

    Adicionando DDL

--- Banco de Dados/01_CRIACAO_BD.sql
+++ Banco de Dados/01_CRIACAO_BD.sql
@@ -2,3 +2,29 @@
 
  USE Senatur_Tarde;
 
+ CREATE TABLE Pacotes (
+	IdPacote		INT PRIMARY KEY IDENTITY,
+	NomePacote		VARCHAR(200),
+	Descricao		VARCHAR(2000),
+	DataIda			DATE,
+	DataVolta		DATE,
+	Valor			MONEY,
+	Ativo			BIT NOT NULL,
+	NomeCidade		VARCHAR(200)
+);
+
+CREATE TABLE TipoUsuario (
+	IdTipoUsuario		INT PRIMARY KEY IDENTITY,
+	Titulo				VARCHAR(200) NOT NULL UNIQUE				
+);
+
+CREATE TABLE Usuarios (
+	IdUsuario		INT PRIMARY KEY IDENTITY,
+	Email			VARCHAR(200) NOT NULL UNIQUE,
+	Senha			VARCHAR(100) NOT NULL,
+	IdTipoUsuario	INT FOREIGN KEY REFERENCES TipoUsuario(IdTipoUsuario)
+);
+
+
+
+
