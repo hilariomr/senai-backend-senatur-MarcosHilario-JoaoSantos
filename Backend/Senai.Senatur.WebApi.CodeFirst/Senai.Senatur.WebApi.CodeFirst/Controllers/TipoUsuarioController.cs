75bf44ec73bcd165c1bfb4dc4ccc51667b15e37b
Marcos Hilario <mrhilario1985@gmail.com>
2020-03-11 14:10:16 -0300

    Inserindo TipoUsuario

---
+++ Backend/Senai.Senatur.WebApi.CodeFirst/Senai.Senatur.WebApi.CodeFirst/Controllers/TipoUsuarioController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Senai.Senatur.WebApi.CodeFirst.Domains;
+using Senai.Senatur.WebApi.CodeFirst.Interfaces;
+using Senai.Senatur.WebApi.CodeFirst.Repositories;
+
+namespace Senai.Senatur.WebApi.CodeFirst.Controllers
+{
+
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TipoUsuarioController : ControllerBase
+    {
+
+        private ITipoUsuarioRepository _tipoUsuarioRepository;
+
+        public TipoUsuarioController()
+        {
+            _tipoUsuarioRepository = new TipoUsuarioRepository();
+        }
+
+        [HttpGet]
+
+        public IActionResult Get()
+        {
+            return Ok(_tipoUsuarioRepository.Listar());
+        }
+
+        [HttpGet("{id}")]
+
+        public IActionResult GetById(int id)
+        {
+            return Ok(_tipoUsuarioRepository.BuscarPorId(id));
+        }
+
+        [HttpDelete("(id)")]
+        public IActionResult Delete(int id)
+        {
+            _tipoUsuarioRepository.Deletar(id);
+            return StatusCode(200);
+        }
+
+        [HttpPost]
+        public IActionResult Post(TipoUsuario novoTipoUsuario)
+        {
+            _tipoUsuarioRepository.Cadastrar(novoTipoUsuario);
+            return StatusCode(200);
+        }
+
+        [HttpPut]
+        public IActionResult Put(TipoUsuario addTipoUsuario)
+        {
+            _tipoUsuarioRepository.Atualizar(addTipoUsuario);
+            return StatusCode(200);
+        }
+
+
+
+
+    }
+}
---
+++ Backend/Senai.Senatur.WebApi.CodeFirst/Senai.Senatur.WebApi.CodeFirst/Repositories/TipoUsuarioRepository.cs
@@ -0,0 +1,47 @@
+﻿using Senai.Senatur.WebApi.CodeFirst.Domains;
+using Senai.Senatur.WebApi.CodeFirst.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.Senatur.WebApi.CodeFirst.Repositories
+{
+    public class TipoUsuarioRepository : ITipoUsuarioRepository
+    {
+
+        SenaturContext ctx = new SenaturContext();
+
+
+        
+
+        public void Atualizar(TipoUsuario addtipoUsuario)
+        {
+            ctx.TipoUsuario.Update(addtipoUsuario);
+            ctx.SaveChanges();
+        }
+
+        public TipoUsuario BuscarPorId(int id)
+        {
+            return ctx.TipoUsuario.FirstOrDefault(e => e.IdTipoUsuario == id);
+        }
+
+        public void Cadastrar(TipoUsuario novoTipoUsuario)
+        {
+            ctx.TipoUsuario.Add(novoTipoUsuario);
+            ctx.SaveChanges();
+        }
+
+        public void Deletar(int id)
+        {
+            var deleteItem = ctx.TipoUsuario.FirstOrDefault(e => e.IdTipoUsuario == id);
+            ctx.TipoUsuario.Remove(deleteItem);
+            ctx.SaveChanges();
+        }
+
+        public List<TipoUsuario> Listar()
+        {
+            return ctx.TipoUsuario.ToList();
+        }
+    }
+}
